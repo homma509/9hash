5e6843d924983ef68214d72dc481e770c1274ab6
homma509 <kouki.honma@gmail.com>
2020-04-30 00:20:24 +0900

    rename directory bin to build

--- scripts/go-build.sh
+++ scripts/go-build.sh
@@ -3,4 +3,4 @@
 cd adapter/handler
 find . -name main.go -type f \
  | xargs -n 1 dirname \
- | xargs -n 1 -I@ bash -c "CGO_ENABLED=0 GOOS=linux go build -v -installsuffix cgo -o ../../bin/@/main ./@"
+ | xargs -n 1 -I@ bash -c "CGO_ENABLED=0 GOOS=linux go build -v -installsuffix cgo -o ../../build/@/main ./@"
