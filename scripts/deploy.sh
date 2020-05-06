fc53fba920c44a8a9f27dd4745f4edf270356b08
homma509 <kouki.honma@gmail.com>
2020-05-06 15:20:32 +0900

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,29 +2,16 @@
 
 npm install
 
-ENV="development"
-STAGE="dev"
-BACKEND=TRUE
+SYSTEM=${1:-backend}
+STAGE=${2:-dev}
 
-while [ $# -gt 0 ]
-do
-    case "$1" in
-        "dev" ) ENV="development"
-                STAGE="dev"
-                ;;
-        "staging" ) ENV="staging"
-                    STAGE="staging"
-                    ;;
-        "prod" ) ENV="production"
-                 STAGE="prod"
-                 ;;
-        "frontend" ) BACKEND=FALSE
-                     ;;
-    esac
-    shift
-done
+case "$STAGE" in
+    "dev" )     ENV="development"   ;;
+    "staging" ) ENV="staging"       ;;
+    "prod" )    ENV="production"    ;;
+esac
 
-if [ $BACKEND = TRUE ]; then
+if [ "$SYSTEM" = "backend" ]; then
     sls deploy --env $ENV --stage $STAGE --config serverless_go.yml
     API_URL=$(sls info --stage $STAGE --config serverless_go.yml --verbose | sed -n -r 's/^.*ServiceEndpoint: (.+)/\1/p')
     echo -n -e "NODE_ENV=$ENV\nVUE_APP_API_BASE_URL=$API_URL/v1" > ./web/app/.env
